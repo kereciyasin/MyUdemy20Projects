7fb219acbf3b2829e402179d4381de1a468021ed
Yasin Kereci <kereciyasin52@gmail.com>
2024-09-26 16:40:34 +0200

    update

--- Project1_AdonetCustomer/Project1_AdonetCustomer/Form1.cs
+++ Project1_AdonetCustomer/Project1_AdonetCustomer/Form1.cs
@@ -21,10 +21,14 @@ namespace Project1_AdonetCustomer
 
         private void btnView_Click(object sender, EventArgs e)
         {
-            SqlConnection sqlConnection = new SqlConnection("Server=KERECI\\SQLEXPRESS; initial catalog=DbCustomer; integrated security=true");   
+            SqlConnection sqlConnection = new SqlConnection("Server=KERECI\\SQLEXPRESS; Initial Catalog=DbCustemer; Integrated Security=True;\r\n");   
 
             sqlConnection.Open();
-            SqlCommand sqlCommand = new SqlCommand("Select * from TblCustomer", sqlConnection);
+            SqlCommand sqlCommand = new SqlCommand("Select * from Tbl_Customer", sqlConnection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;   
             sqlConnection.Close();
         }
     }
